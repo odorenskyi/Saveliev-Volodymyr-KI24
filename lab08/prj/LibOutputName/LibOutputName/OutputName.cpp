cb41b0646f4ab045494c81587b924fb559b11ea1
vovanhik1342 <vovasavelev099@gmail.com>
2025-04-14 22:16:24 +0300

    Створено бібліотеку LibOutputName. Реалізовано функцію виводу імені розробника.

---
+++ lab08/prj/LibOutputName/LibOutputName/OutputName.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void Information() {
+    SetConsoleOutputCP(1251);
+    cout << "\n ----------------------------------------\n"
+
+        << "|   Volodymyr Cavelyev, CUNTU, Kropyvnickyi   |\n"
+
+        << "|  Володимр Савельєв, ЦНТУ, Кропивницький   |\n"
+
+        << "\n -------- (c) All Rights Reserved --------\n\n";
+}
+
