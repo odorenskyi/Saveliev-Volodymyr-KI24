9fb9c777a6ef06dd02140a872302aa11484a98d8
vovanhik1342 <vovasavelev099@gmail.com>
2025-05-01 22:18:59 +0300

    Task 9.1: реалізовано ф-ціїї (умова задачі 9.1) в ModulesSavelyev

---
+++ lab09/prj/ModulesSovelyev/ModulesCovelyev.h
@@ -0,0 +1,3 @@
+#pragma once
+double s_calculation(double x, double y, double z);
+double CalculateSalary(double baseSalary, double& tax, double& pensionFund, double& unemploymentFund, double& reserveFund, double& militaryTax);
